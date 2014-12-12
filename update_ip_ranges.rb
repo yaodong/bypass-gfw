c7bf55fb17b2531d118390f359952c74d2f122d0
Yaodong Zhao <yaodong.zhao@outlook.com>
2014-12-13 02:56:34 +0800

    remove domain collecting script

--- update_ip_ranges.rb
+++ update_ip_ranges.rb
@@ -5,18 +5,18 @@ require 'open-uri'
 ip_ranges  = {}
 
 # aws
-p 'collecting aws ip ranges'
+puts 'collecting aws ip ranges'
 aws_ranges = JSON.parse open('https://ip-ranges.amazonaws.com/ip-ranges.json').read
 ip_ranges['aws'] = aws_ranges['prefixes'].map { |p| p['ip_prefix'] }
 
 # cloudflare cdn
-p 'collecting cloudflare ip ranges'
+puts 'collecting cloudflare ip ranges'
 ip_ranges['cloudflare'] = open('https://www.cloudflare.com/ips-v4').read.split("\n")
 
 # ip ranges by ASN
 asn = File.read("#{__dir__}/conf/asn.list").split("\n").map { |n| n.split /\s+/ }
 asn.each do |n|
-  p "collecting #{n[1]} #{n[0]} ranges"
+  puts "collecting #{n[1]} #{n[0]} ranges"
   raw_data  = `whois -h whois.radb.net -- '-i origin #{n[0]}' | grep route:`
   ranges    = raw_data.split("\n").map { |i| i.split(/\s+/).last }
   loop do
