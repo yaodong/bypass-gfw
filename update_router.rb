90ca07a9b84be0b22d6ba62072ecaed9a7b3a16a
Aveline Swan <i@xswan.net>
2015-01-09 15:26:04 +0800

    replace NetAddr with cidr-lite to get fewer routes

--- update_router.rb
+++ update_router.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 
-require 'netaddr'
 require 'json'
+require 'cidr-lite'
 
 vpn_gateway = 'pptp-out1'
 
@@ -10,15 +10,15 @@ vpn_gateway = 'pptp-out1'
 # -------------------------------------------------------------------
 ip_ranges = Dir.glob("#{__dir__}/data/ip_ranges/*.json").map { |f| JSON.parse File.read(f) }.flatten
 ip_ranges.concat JSON.parse(File.read("#{__dir__}/data/domains.json")).values
-loop do
-  length    = ip_ranges.length
-  ip_ranges = NetAddr.merge ip_ranges
-  break if ip_ranges.length == length
-end
+
+lite = CIDR::Lite.new
+ip_ranges.each {|v| 
+	lite.add v
+}
 
 # -------------------------------------------------------------------
 # Store IP ranges
 # -------------------------------------------------------------------
 rules = ['/ip route remove [/ip route find gateway=pptp-out1 static=no]']
-rules.concat ip_ranges.map{ |i| "/ip route add dst-address=#{i} gateway=#{vpn_gateway}" }
+rules.concat lite.list.map{ |i| "/ip route add dst-address=#{i} gateway=#{vpn_gateway}" }
 File.write "#{__dir__}/router/router.txt", rules.join("\n")
