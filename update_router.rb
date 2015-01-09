7a3fe44499e6694094f66921bf5d71e0ba4be915
Yaodong Zhao <yaodong.zhao@outlook.com>
2015-01-09 21:28:38 +0800

    add comment for RouterOS routes

--- update_router.rb
+++ update_router.rb
@@ -5,6 +5,6 @@ require 'json'
 vpn_gateway = 'pptp-out1'
 ip_ranges   = JSON.parse File.read("#{__dir__}/data/ip_ranges.json")
 
-rules = ['/ip route remove [/ip route find gateway=pptp-out1 static=no]']
-rules.concat ip_ranges.map{ |i| "/ip route add dst-address=#{i} gateway=#{vpn_gateway}" }
+rules = ['/ip route remove [/ip route find gateway=pptp-out1 comment=gfw-butter]']
+rules.concat ip_ranges.map{ |i| "/ip route add dst-address=#{i} gateway=#{vpn_gateway} comment=gfw-butter" }
 File.write "#{__dir__}/router/router.txt", rules.join("\n")
